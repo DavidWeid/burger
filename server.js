7e74c068b8339d1c90570a44b0e37a6aec4f980e
David Weid <david.weid.2@gmail.com>
2019-03-01 13:35:05 -0500

    update

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//////////////////////////////////////////////////
+
 // Dependencies //
 var express = require("express");
 var exphbs = require("express-handlebars");
@@ -14,13 +16,13 @@ var connection = mysql.createConnection({
     database: "burgers_db"
 });
 
-// Middleware configuration
+// Middleware configuration //
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Connection to database and server
+// Connection to database and server //
 connection.connect(function(err) {
     if (err) throw err;
 
@@ -30,3 +32,30 @@ connection.connect(function(err) {
 app.listen(PORT, function() {
     console.log("Server online at http://localhost:%s", PORT);
 });
+
+//////////////////////////////////////////////////
+
+// Use handlebars to render main index.html page
+app.get("/", function(req, res) {
+    connection.query("SELECT * FROM burgers;", function(err, data) {
+        if (err) { return res.status(500).end(); };
+
+        console.log(data);
+
+        res.render("index", { burgers: data });
+    });
+});
+
+app.post("/", function(req, res) {
+    connection.query("INSER INTO burgers (burger_name) VALUES (?)", [req.body.burger], function(err, result) {
+        if (err) { return res.status(500).end(); };
+
+    });
+});
+
+app.put("/", function(req, res) {
+    connection.query("UPDATE burgers SET burger_name = ? WHERE id = ?", [req.body.plan, req.params.id], function(err, result) {
+        if (err) { return res.status(500).end(); };
+        
+    });
+})
